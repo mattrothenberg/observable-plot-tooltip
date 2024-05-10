5d7c65b49dd2946a2319d07a87f5880ca2aeae73
Matt Rothenberg <mattrothenberg@gmail.com>
2024-05-10 16:15:20 -0400

    add badge

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
       <div className="h-[200px]">
         <SpendOverTime />
       </div>
+      <div className="fixed bottom-4 right-4">
+        <a
+          className="text-sm inline-flex border-2 border-green-9 rounded-full px-4 py-1.5 text-green-11 hover:shadow-xl hover:shadow-green-a3 font-medium hover:bg-green-a4 transition-all"
+          href="https://github.com/mattrothenberg/observable-plot-tooltip"
+        >
+          View GitHub repo
+        </a>
+      </div>
     </div>
   );
 }
